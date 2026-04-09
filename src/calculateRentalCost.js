cec338c054d9bf689025c719f1c95ade91802126
Mariana <zh.yu.st22@gmail.com>
2026-04-10 00:48:20 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,6 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  if (days < 3 && days > 0) {
+    return days * 40;
+  } else if (days >= 3 && days <= 6) {
+    return days * 40 - 20;
+  } else if (days >= 7) {
+    return days * 40 - 50;
+  } else {
+    return 'invalide date';
+  }
+
   // write code here
 }
 
