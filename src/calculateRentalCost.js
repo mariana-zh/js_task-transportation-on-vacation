5665667e165d506176f5d6b675c2066bf384c5f6
Mariana <zh.yu.st22@gmail.com>
2026-04-10 00:58:48 +0300

    add task solution 2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,14 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const costPerDay = 40;
+  const bigDiscount = 50;
+  const smallDiscount = 20;
+
   if (days < 3 && days > 0) {
-    return days * 40;
+    return days * costPerDay;
   } else if (days >= 3 && days <= 6) {
-    return days * 40 - 20;
+    return days * costPerDay - smallDiscount;
   } else if (days >= 7) {
-    return days * 40 - 50;
+    return days * costPerDay - bigDiscount;
   } else {
-    return 'invalide date';
+    return 0;
   }
 
   // write code here
